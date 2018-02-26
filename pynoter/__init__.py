cc588047631e7a13de46e1ee31f5ff8f10388588
maxime <max@tpd-x220>
2018-02-25 19:36:03 -0500

    enabling scripting and JSON incorporation

---
+++ pynoter/__init__.py
@@ -0,0 +1,23 @@
+import conv_raw
+import argparse
+import os
+
+# make argument parser for powerpoint and methods to be called
+# on the powerpoint
+ap = argparse.ArgumentParser()
+ap.add_argument('-p', '--path', required=True, help='path to ppt')
+ap.add_argument('-t', '--title', required=False, help='title new doc')
+ap.add_argument('-ft', '--filetype', required=True, help='specify type of file')
+
+args = vars(ap.parse_args())
+
+path = str(args['path'])
+title = str(args['title'])
+filetype = str(args['filetype'])
+
+
+# call each method depending on the boolean value attributed
+def main():
+    myppt = conv_raw.Powerpoint(path, title, filetype)
+    myppt.slide_note()
+    myppt.note_file()
